1afc0f49b8386d43295899981bb123bc5e95e2da
CizoDev <jaydeepviranicizo@gmail.com>
2018-12-06 18:46:10 +0530

    Update twillo.js
    Make hidden credentials

--- config/twillo.js
+++ config/twillo.js
@@ -1,4 +1,4 @@
-const accountSid = 'ACa50e837ad350ed6c3874dd568bc28d87';
-const authToken = 'fca00b2bedf1a41876de71a38b9dafd0';
+const accountSid = '***************';
+const authToken = '*******************';
 const client = require('twilio')(accountSid, authToken);
 module.exports = client;

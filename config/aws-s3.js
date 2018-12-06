30312f69b58f0959eedd9d6c70588031fc885d03
CizoDev <jaydeepviranicizo@gmail.com>
2018-12-06 11:05:52 +0530

    Fixed AWS security

--- config/aws-s3.js
+++ config/aws-s3.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
-    accessKeyId: 'AKIAJ6FDVKKPWTY6GEEQ',
-    secretAccessKey: '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'
+    accessKeyId: '***************',
+    secretAccessKey: '**********************'
 });
 
 module.exports = s3;
